ba61644c13e51ba5d0d0a291520a5c0b60ddf9bf
Preston Bruce <pb.itoldyouso@gmail.com>
2018-08-20 16:39:39 -0400

    Added comments

--- test.java
+++ test.java
@@ -1,7 +1,10 @@
+/*
+This is a template file for the purpose of creating new files
+*/
 import java.util.*;
 
 public test{
   public static void main(String[] args) {
-
+    Scanner input = new Scanner(System.in);
   }
 }
