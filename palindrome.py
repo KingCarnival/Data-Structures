2a24970366927a3bb4d867b0616b6abce5ad3bac
Preston Bruce <pb.itoldyouso@gmail.com>
2018-08-23 10:36:55 -0400

    corrections and examples

--- palindrome.py
+++ palindrome.py
@@ -2,5 +2,12 @@ def palindrome(text):
     questionWord = list(text)
     if questionWord == questionWord.reverse():
         print("This is a palindrome")
-    else
+    else:
         print("This is not a palindrome")
+
+first = "true"
+palindrome(first)
+second = "false"
+palindrome(second)
+third = "bob"
+palindrome(third)
