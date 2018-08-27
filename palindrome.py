3165f5c9f79b21e20435fb9768381bd89f867e78
Preston Bruce <pb.itoldyouso@gmail.com>
2018-08-27 11:13:51 -0400

    adding code comments

--- palindrome.py
+++ palindrome.py
@@ -1,10 +1,10 @@
-def palindrome(text):
+def palindrome(text): #Creating the function
     questionWord = list(text)
     testWord = list(text)
-    testWord.reverse()
+    testWord.reverse() #reverses the word list to test
     print(questionWord)
     print(testWord)
-    if questionWord == testWord:
+    if questionWord == testWord: #checks if the list are equal
         print("This is a palindrome")
     else:
         print("This is not a palindrome")
