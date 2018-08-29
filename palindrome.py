599b9a8e7ccef5beb6b33d01c5fc8bd0b026acc1
Preston Bruce <pb.itoldyouso@gmail.com>
2018-08-29 17:01:35 -0400

    name changes

--- palindrome.py
+++ palindrome.py
@@ -2,11 +2,11 @@
 #Palindrome 
 def palindrome(text): #Creating the function
     questionWord = list(text)
-    testWord = list(text)
-    testWord.reverse() #reverses the word list to test
+    reverseWord = list(text)
+    reverseWord.reverse() #reverses the word list to test
     print(questionWord)
-    print(testWord)
-    if questionWord == testWord: #checks if the list are equal
+    print(reverseWord)
+    if questionWord == reverseWord: #checks if the list are equal
         print("This is a palindrome")
     else:
         print("This is not a palindrome")
